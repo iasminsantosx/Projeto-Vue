e644f56250458d5fc6d63715cfb0c52beae7c31d
Iasmin Santos <iasminwinx@gmail.com>
2022-03-16 15:10:16 -0300

    Primeiro commit do repo

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <AppTopBar/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppTopBar from './components/shared/AppTopBar'
+
+export default {
+  name: 'App',
+  components: {
+    AppTopBar
+  }
+};
+</script>
+
+<style>
+.link-home {
+  color: white !important;
+  text-decoration: none;
+}
+</style>
